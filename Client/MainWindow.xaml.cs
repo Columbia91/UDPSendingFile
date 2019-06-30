6589fe13005ab0e2c1237ebc54befcf8f777cd97
ADIL <globalmail91@gmail.com>
2019-07-01 00:30:37 +0600

    Добавьте файлы проекта.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+
+namespace Client
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string fileName;
+        private string filePath;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "Текстовые файлы|*.txt|Все файлы|*.*";
+                if (dialog.ShowDialog() == true)
+                {
+                    using (FileStream fileStream = new FileStream(dialog.FileName, FileMode.Open))
+                    {
+                        fileName = System.IO.Path.GetFileName(dialog.FileName);
+                        filePath = filePathTextBox.Text = dialog.FileName;
+                    }
+                }
+            }
+            catch (ArgumentException exception)
+            {
+                MessageBox.Show(exception.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(ipTextBox.Text) && !string.IsNullOrWhiteSpace(portTextBox.Text)
+                && !string.IsNullOrWhiteSpace(blockSizeTextBox.Text)
+                && !string.IsNullOrWhiteSpace(filePathTextBox.Text))
+            {
+                try
+                {
+                    Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+                    string ipServer = ipTextBox.Text;
+                    int port = int.Parse(portTextBox.Text);
+                    EndPoint srvEP = new IPEndPoint(IPAddress.Parse(ipServer), port);
+
+                    client.SendTo(Encoding.UTF8.GetBytes(fileName), srvEP);
+
+                    using (FileStream stream = new FileStream(filePath, FileMode.OpenOrCreate))
+                    {
+                        long cntRecive = stream.Length / int.Parse(blockSizeTextBox.Text);
+                        long remainder = stream.Length % int.Parse(blockSizeTextBox.Text);
+
+                        if (remainder > 0) cntRecive++;
+                        client.SendTo(BitConverter.GetBytes((int)cntRecive), srvEP);
+
+                        byte[] buffer = new byte[int.Parse(blockSizeTextBox.Text)];
+
+                        for (int i = 0; i < (remainder > 0 ? cntRecive - 1 : cntRecive); i++)
+                        {
+                            stream.Read(buffer, 0, int.Parse(blockSizeTextBox.Text));
+                            client.SendTo(buffer, srvEP);
+                        }
+
+                        if (remainder > 0)
+                        {
+                            stream.Read(buffer, 0, (int)remainder);
+                            client.SendTo(buffer, 0, (int)remainder, SocketFlags.None, srvEP);
+                        }
+                    }
+                }
+                catch (ArgumentNullException ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                catch (SocketException ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+                MessageBox.Show("Не все поля заполнены", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
---
+++ DestinationServer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace DestinationServer
+{
+    class Program
+    {
+        static Socket srvSock;
+        static string ipServer = "0.0.0.0";
+        static int portServer = 12345;
+        static void Main(string[] args)
+        {
+            srvSock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            srvSock.Bind(new IPEndPoint(IPAddress.Parse(ipServer), portServer));
+            EndPoint ClientEndPoint = new IPEndPoint(0, 0);
+
+            byte[] buffer = new byte[64*1024];
+            // получить имя файла
+            int recSize = srvSock.ReceiveFrom(buffer, ref ClientEndPoint);
+            string FileName = Encoding.UTF8.GetString(buffer, 0, recSize);
+            Console.WriteLine("File name: " + FileName);
+
+            // Папка Документы
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\" + FileName;
+            using (FileStream outFile = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                // получить количество посылок
+                srvSock.ReceiveFrom(buffer, ref ClientEndPoint);
+                int cntRecive = BitConverter.ToInt32(buffer, 0);
+                Console.WriteLine("Blocks count: " + cntRecive);
+
+                // прием частей файла
+                int fullSize = 0;
+                for (int i = 0; i < cntRecive; i++)
+                {
+                    recSize = srvSock.ReceiveFrom(buffer, ref ClientEndPoint);
+                    if (recSize > 0)
+                    {
+                        Console.WriteLine($"{i+1}) Receive {recSize} bytes   \r");
+                        outFile.Write(buffer, 0, recSize);
+                        fullSize += recSize;
+                    }
+                    else
+                        break;
+                }
+                Console.WriteLine("\nFull size of file = {0}", fullSize);
+                //outFile.Flush(true); // принудительный сброс данных
+                //outFile.Close(); принудительное закрытие потока
+            }
+        }
+    }
+}
